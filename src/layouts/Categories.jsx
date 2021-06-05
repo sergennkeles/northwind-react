323dca18ed50cf9f4401342b3a37f76e2b763652
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-06-06 02:22:07 +0300

    Categories added

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { Menu } from "semantic-ui-react";
+import CategoryService from "../services/categoryService";
 
 export default function Categories() {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(()=>{
+    let categoryService=new CategoryService();
+    categoryService.getCategories().then(result=>setCategories(result.data.data),[])
+  })
   return (
     <div>
-      <Menu pointing vertical>
-        <Menu.Item name="home" />
-        <Menu.Item name="messages" />
-        <Menu.Item name="friends" />
-         <Menu.Item name="friends" />
-          <Menu.Item name="friends" />
-           <Menu.Item name="friends" />
+      <Menu pointing vertical >
+        {
+          categories.map((category)=>(
+            <Menu.Item key={category.categoryId} name={category.categoryName} />
+          ))
+        }
+     
       </Menu>
     </div>
   );
